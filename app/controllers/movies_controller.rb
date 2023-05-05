34671cd508550b79f9407a98056593e0aefa462d
Augusto Arraes <augustobarraes@gmail.com>
2023-05-05 00:47:53 -0400

    working on delete functionality

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,43 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+    @movie = Movie.find(params[:id])
+  end
+
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+
+
+    @movie.update(permitted_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new()
+  end
+
+  def create
+    @movie = Movie.create(permitted_params)
+    redirect_to @movie, notice: "Movie created successfully"
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.delete
+    redirect_to movies_url, status: :see_other
+  end
+
+  private
+
+  def permitted_params
+    params.require(:movie).permit(:title, :description, :rating, :released_on, :total_gross)
+  end
+
+end
